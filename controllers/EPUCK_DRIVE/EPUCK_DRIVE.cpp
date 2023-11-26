985822055865280c66dfd6a24cd71cf70e5d9add
a-hamzah <Spellbreaker907@gmail.com>
2023-11-26 10:59:57 +0500

    Minor Changes

--- controllers/EPUCK_DRIVE/EPUCK_DRIVE.cpp
+++ controllers/EPUCK_DRIVE/EPUCK_DRIVE.cpp
@@ -9,6 +9,9 @@
 // and/or to add some other includes
 #include <webots/Robot.hpp>
 #include <webots/Motor.hpp>
+#include <webots/DistanceSensor.hpp>
+
+
 #define TIME_STEP 64
 #define MAX_SPEED 6.28
 // All the webots classes are defined in the "webots" namespace
@@ -24,8 +27,15 @@ using namespace webots;
 int main(int argc, char **argv) {
   // create the Robot instance.
   Robot *robot = new Robot();
+  // Motors
   Motor *leftMotor = robot->getMotor("left wheel motor");
   Motor *rightMotor = robot->getMotor("right wheel motor");
+  // Distance Sensor
+  DistanceSensor *ds_left = robot->getDistanceSensor("ps7");
+  DistanceSensor *ds_right = robot->getDistanceSensor("ps0");
+  // Enable distance sensor using time step
+  ds_left->enable(TIME_STEP);
+  ds_right->enable(TIME_STEP);
   
   leftMotor->setPosition(INFINITY);
   rightMotor->setPosition(INFINITY);
@@ -45,8 +55,15 @@ int main(int argc, char **argv) {
   // Main loop:
   // - perform simulation steps until Webots is stopping the controller
   while (robot->step(TIME_STEP) != -1) {
-    leftMotor->setVelocity(MAX_SPEED/2);
+    double ds_left_val = ds_left->getValue();
+    double ds_right_val = ds_right->getValue();
+    
+    std::cout<< "Distance Sensor Left: " << ds_left_val << std::endl;
+    std::cout<< "Distance Sensor Right: " << ds_right_val << std::endl;
+    
+    leftMotor->setVelocity(MAX_SPEED/4);
     rightMotor->setVelocity(MAX_SPEED/4);
+    // rightMotor->setVelocity(-MAX_SPEED/2);
     // Read the sensors:
     // Enter here functions to read sensor data, like:
     //  double val = ds->getValue();
